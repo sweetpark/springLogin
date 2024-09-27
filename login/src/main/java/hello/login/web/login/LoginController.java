d9be8a3ad976aabf1a4017c1c99e6ca1ca35c144
wooyeong <wooyeong@wooyeongui-MacBookPro.local>
2024-09-27 16:51:07 +0900

    Update Filter

--- login/src/main/java/hello/login/web/login/LoginController.java
+++ login/src/main/java/hello/login/web/login/LoginController.java
@@ -12,6 +12,7 @@ import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
@@ -84,7 +85,7 @@ public class LoginController {
     }
 
 
-    @PostMapping("/login")
+    //@PostMapping("/login")
     public String loginV3(@Valid @ModelAttribute("loginForm") LoginForm form, BindingResult bindingResult, HttpServletRequest request){
 
         if (bindingResult.hasErrors()){
@@ -109,6 +110,33 @@ public class LoginController {
         return "redirect:/";
     }
 
+    @PostMapping("/login")
+    public String loginV4(@Valid @ModelAttribute("loginForm") LoginForm form, BindingResult bindingResult,
+                          @RequestParam(defaultValue = "/") String redirectURL,
+                          HttpServletRequest request){
+
+        if (bindingResult.hasErrors()){
+            return "login/loginForm";
+        }
+
+        Member loginMember = loginService.login(form.getLoginId(), form.getPassword());
+
+        //DB까지 확인후 해야하기 떄문에 -> 필드오류 (fieldError) 로 사용못하고, ObjectError를 이용해야한다 (reject())
+        if(loginMember == null){
+            bindingResult.reject("loginFail", "아이디 또는 비밀번호가 맞지 않습니다.");
+            return "login/loginForm";
+        }
+
+
+        //로그인 성공 처리 TODO -> 쿠키를 만들어서 client 로 전달
+        //세션이 있으면 세션 반환, 세션이 없으면 신규 세션 생성
+        HttpSession session = request.getSession();
+        //세션에 로그인 회원 정보 보관
+        session.setAttribute(SessionConst.LOGIN_MEMBER, loginMember);
+
+
+        return "redirect:" + redirectURL;
+    }
 
     //@PostMapping("/logout")
     public String logout(HttpServletResponse response){
