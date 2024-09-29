86b4f1cd33eb26a95f2a61de8e73f0138f9126db
wooyeong <wooyeong@wooyeongui-MacBookPro.local>
2024-09-29 13:53:55 +0900

    Interceptor + ArgumementResolver 추가

--- login/src/main/java/hello/login/web/HomeController.java
+++ login/src/main/java/hello/login/web/HomeController.java
@@ -2,6 +2,7 @@ package hello.login.web;
 
 import hello.login.domain.member.Member;
 import hello.login.domain.member.MemberRepository;
+import hello.login.web.argumentresolver.Login;
 import hello.login.web.session.SessionManager;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -81,8 +82,21 @@ public class HomeController {
 
 
     //@SessionAttribute는 세션을 생성하지는 않는다
-    @GetMapping("/")
+    //@GetMapping("/")
     public String homeLoginV3Spring(@SessionAttribute(name=SessionConst.LOGIN_MEMBER, required = false) Member loginMember, Model model){
+
+        //세션에 회원 데이터가 없으면 home
+        if(loginMember ==null){
+            return "home";
+        }
+
+        //세션이 유지되면 View
+        model.addAttribute("member", loginMember);
+        return "loginHome";
+    }
+
+    @GetMapping("/")
+    public String homeLoginV3ArgumentResolver(@Login Member loginMember, Model model){
 
         //세션에 회원 데이터가 없으면 home
         if(loginMember ==null){
